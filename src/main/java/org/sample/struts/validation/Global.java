d1e504151f9a9539a6c4759b189507341a10c3de
sample <sample@example.com>
2018-03-21 16:30:55 +0900

    test

---
+++ src/main/java/org/sample/struts/validation/Global.java
@@ -0,0 +1,76 @@
+//
+// このファイルは、JavaTM Architecture for XML Binding(JAXB) Reference Implementation、v2.2.8-b130911.1802によって生成されました 
+// <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>を参照してください 
+// ソース・スキーマの再コンパイル時にこのファイルの変更は失われます。 
+// 生成日: 2018.03.21 時間 01:39:11 PM JST 
+//
+
+
+package org.sample.struts.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>anonymous complex typeのJavaクラス。
+ * 
+ * <p>次のスキーマ・フラグメントは、このクラス内に含まれる予期されるコンテンツを指定します。
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element ref="{}constant" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "constant"
+})
+@XmlRootElement(name = "global")
+public class Global {
+
+    protected List<Constant> constant;
+
+    /**
+     * Gets the value of the constant property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the constant property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getConstant().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Constant }
+     * 
+     * 
+     */
+    public List<Constant> getConstant() {
+        if (constant == null) {
+            constant = new ArrayList<Constant>();
+        }
+        return this.constant;
+    }
+
+}
