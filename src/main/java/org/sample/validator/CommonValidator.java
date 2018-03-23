50b75f4e756b74c9f9af6160aa705eb05eed3d88
sample <sample@example.com>
2018-03-23 16:34:56 +0900

    t

--- src/main/java/org/sample/validator/CommonValidator.java
+++ src/main/java/org/sample/validator/CommonValidator.java
@@ -1,37 +1,58 @@
 package org.sample.validator;
 
+import org.sample.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class CommonValidator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommonValidator.class);
 
     private CommonValidator() {
     }
 
     public static final boolean validateMaxLength(Object object, int max) {
-        String value = toString(object);
+        String value = StringUtils.toStringValue(object);
         boolean isValid = true;
         if (value != null) {
             isValid = (value.length() <= max);
         }
         return isValid;
-
     }
 
     public static final boolean validateMinLength(Object object, int min) {
-        String value = toString(object);
+        String value = StringUtils.toStringValue(object);
         boolean isValid = true;
-        if (!isBlankOrNull(value)) {
+        if (!StringUtils.isBlankOrNull(value)) {
             isValid = (value.length() >= min);
         }
         return isValid;
     }
 
-    public static final boolean validateIntRange(Object object, int min, int max) {
-        String value = toString(object);
+    public static final boolean validateInteger(Object object) {
+        String value = StringUtils.toStringValue(object);
         boolean isValid = true;
-        if (!isBlankOrNull(value)) {
+        if (!StringUtils.isBlankOrNull(value)) {
+            try {
+                Integer.valueOf(value);
+            } catch (Exception e) {
+                LOGGER.debug("error occurred validateInteger.", e);
+                isValid = false;
+            }
+        }
+        return isValid;
+    }
+
+    public static final boolean validateIntRange(Object object, int min, int max) {
+        String value = StringUtils.toStringValue(object);
+        boolean isValid = true;
+        if (!StringUtils.isBlankOrNull(value)) {
             try {
                 int intValue = Integer.valueOf(value);
                 isValid = ((intValue >= min) && (intValue <= max));
             } catch (Exception e) {
+                LOGGER.debug("error occurred validateIntRange.", e);
                 isValid = false;
             }
         }
@@ -39,37 +60,21 @@ public class CommonValidator {
     }
 
     public static final boolean validateDouble(Object object) {
-        String value = toString(object);
+        String value = StringUtils.toStringValue(object);
         boolean isValid = true;
         try {
-            if (!isBlankOrNull(value)) {
+            if (!StringUtils.isBlankOrNull(value)) {
                 Double.valueOf(value);
             }
-        } catch (NumberFormatException | NullPointerException e) {
+        } catch (NumberFormatException e) {
+            LOGGER.debug("error occurred validateDouble.", e);
             isValid = false;
         }
         return isValid;
     }
 
     public static final boolean validateRequired(Object object) {
-        return !isBlankOrNull(toString(object));
+        return !StringUtils.isBlankOrNull(StringUtils.toStringValue(object));
     }
 
-    public static final boolean isBlankOrNull(String value) {
-        return ((value == null) || (value.trim().length() == 0));
-    }
-
-    private static final String toString(Object object) {
-        String value = null;
-        if (isString(object)) {
-            value = (String) object;
-        } else if (object != null) {
-            value = object.toString();
-        }
-        return value;
-    }
-
-    private static final boolean isString(Object object) {
-        return String.class.isInstance(object);
-    }
 }
