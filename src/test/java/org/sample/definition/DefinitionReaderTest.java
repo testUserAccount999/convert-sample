655161ecbdbd91a05622e0dba6ecfd845d36f6e9
sample <sample@example.com>
2018-03-23 16:34:57 +0900

    t

--- src/test/java/org/sample/definition/DefinitionReaderTest.java
+++ src/test/java/org/sample/definition/DefinitionReaderTest.java
@@ -1,5 +1,25 @@
 package org.sample.definition;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Test;
+
 public class DefinitionReaderTest {
+
+    @Test
+    public void testDefinitionReader() throws Exception {
+        String strutsConfig = "org/sample/struts/config/struts-config.xml";
+        String[] validationXmls = {"org/sample/struts/validation/validation.xml"};
+        String[] validationRules = {"org/sample/struts/rule/validator-rules.xml"};
+        DefinitionReader reader = new DefinitionReader(strutsConfig, validationXmls, validationRules);
+        assertThat(reader , notNullValue());
+        Map<String, ValidatorDefinition> validatorDefinitions = reader.readValidatorDefinition();
+        assertFalse(validatorDefinitions.isEmpty());
+        Map<String, FormDefinition> formDefinition = reader.readFormDefinition();
+        assertFalse(formDefinition.isEmpty());
+    }
 
 }
